6c760d79dfeea789cef7a6c48f6058af81364259
Nikhil Ankam <nikhilankam@nankam.local>
2022-10-24 13:55:34 -0500

    non-blocking I/O seq multi conn

--- main/server.py
+++ main/server.py
@@ -5,18 +5,31 @@ host = 'localhost'
 port = 8989
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((host, port))
     print(f'Started TCP server Host:{host} Port:{port}')
     sock.listen(5)
+    sock.setblocking(False)
 
+    connections = []
     try:
         while True:
-            conn, info = sock.accept()
-            print('Client connected: {info}')
-            with conn:
-                data = conn.recv(1024)
-                while data:
-                    print(data.decode('ascii'))
+            try:
+                conn, info = sock.accept()
+                conn.setblocking(False)
+                print('Client connected: {info}')
+                connections.append(conn)
+            except BlockingIOError:
+                pass
+
+            for conn in connections:
+                try:
                     data = conn.recv(1024)
+                    while data:
+                        print(data)
+                        data = conn.recv(1024) 
+                except BlockingIOError:
+                    pass
     except KeyboardInterrupt:
-        sock.close
+        print('Closing server')
+
